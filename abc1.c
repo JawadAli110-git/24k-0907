72bbbd15c11f26faaee8de4a23964a920171fc06
JawadAli110-git <syed.jawad.zaidi72@gmail.com>
2024-09-04 19:22:12 +0500

    Update abc1.c

--- abc1.c
+++ abc1.c
@@ -4,6 +4,6 @@ int main(void){
     int age=10, marks=90;
     float approx_marks = 89.9;
     char gender[2] = "M";
-    printf("age= %d\nmarks= %d\ngender= %s\napprox_marks= %.1f",age, marks, gender, approx_marks);
+    printf("age= %d\nmarks= %d\ngender= %s\napprox_marks= %.1f\nsize_age=%lu",age, marks, gender, approx_marks, sizeof(age));
     return 0;
 }
