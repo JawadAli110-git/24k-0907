07937958dbcb78707cd86eb4167fe599426898ed
JawadAli110-git <syed.jawad.zaidi72@gmail.com>
2024-10-10 01:11:05 +0500

    New file updated....

---
+++ hr.sql
@@ -0,0 +1,7 @@
+SELECT departments.department_name, AVG(employees.salary) AS average_salary
+FROM employees
+JOIN departments ON employees.department_id = departments.department_id
+GROUP BY departments.department_name;
+
+
+
